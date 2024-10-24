0c66832967eb653e16002dafafd82d9ba099e991
herr27 <128671263+SaliemK@users.noreply.github.com>
2024-10-24 18:05:23 +1100

    Check for win function

--- game.java
+++ game.java
@@ -1,129 +1,134 @@
 
 public class game {
     //board is a 2D array that represents the game board, with 7 columns and 6 rows
-    private String[][] board = new String[7][6];
+    private String[][] board = new String[6][7];
+    private int lastMoveColumn;
+    private int lastMoveRow;
 
     public static void main(String[] args){
         game game = new game();
         game.printBoard();
+        //Game test cases
+        /** 
+        game.insertPiece(7, "X");
+        game.insertPiece(6, "O");
+        game.insertPiece(5, "X");
+        game.insertPiece(4, "X");
+        game.insertPiece(6, "X");
+        game.insertPiece(5, "X");
+        game.insertPiece(5, "X");
+        game.insertPiece(4, "O");
+        game.insertPiece(4, "X");
+        game.insertPiece(4, "X"); */
+        game.printBoard();
+        System.out.println(game.checkWin("X",game.lastMoveColumn,game.lastMoveRow));
+    }
+    public void playAMovePlayer1(int column, String piece){
+        insertPiece(column, piece);
+        printBoard();
+    }
+    public void playAMovePlayer2(int column, String piece){
+        String result = "";
+        insertPiece(column, piece);
+        printBoard();
     }
     public game(){
         //initialize the board with empty spaces
-        for(int i = 0; i < 7; i++){
-            for(int j = 0; j < 6; j++){
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 7; j++){
                 board[i][j] = " ";
             }
-        }
+        }   
     }
     public void printBoard(){
         //print the game board with numbered columns
         for(int i = 0; i < 6; i++){
             for(int j = 0; j < 7; j++){
-                System.out.print(" | " + board[j][i]);
+                System.out.print(" | " + board[i][j]);
             }
             System.out.println(" |");
         }
         System.out.println("   1   2   3   4   5   6   7");
         System.out.println("-------------------------------");
     }
+    public void insertPiece(int column, String piece){
+        //insert a piece into the specified column
+        if(column < 1 || column > 7){
+            System.out.println("Invalid column number. Please enter a number between 1 and 7.");
+        }
+        else{
+            dropPiece(column, piece);
+        }
+    }
 
     //method to drop a piece in a column
     public boolean dropPiece(int column, String piece){
         //find the lowest empty row in the column
         int row = 5;
-        while(board[column-1][row] != " "){
+        while(board[row][column-1] != " "){
             row--;
         }
         //place the piece in the lowest empty row
-        board[column-1][row] = piece;
+        board[row][column-1] = piece;
+        lastMoveColumn = column-1;
+        lastMoveRow = row;
         return true;
     }
-    public boolean checkWinHorizontal(String piece){
+
+    public boolean checkWinHorizontal(String piece, int column, int row){
         //check horizontal
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 6; j++){
-                if(i <= 4 && board[i][j] == piece && board[i+1][j] == piece && board[i+2][j] == piece && board[i+3][j] == piece){
-                    return true;
-                }
-                else if(i>=4 && board[i][j] == piece && board[i-1][j] == piece && board[i-2][j] == piece && board[i-3][j] == piece){
-                    return true;
-                }
-            }
+        if(column <= 3 && board[row][column] == piece && board[row][column+1] == piece && board[row][column+2] == piece && board[row][column+3] == piece){
+            return true;
         }
+        else if(column >=3 && board[row][column] == piece && board[row][column-1] == piece && board[row][column-2] == piece && board[row][column-3] == piece){
+            return true;
+        }
+        
         return false;
     }
-    public boolean checkWinVertical(String piece){
+    public boolean checkWinVertical(String piece, int column, int row){
+        boolean win = false;
+        System.out.println("row: " + row);
         //check vertical
-        for(int i = 0; i < 7; i++){
-            for(int j = 0; j < 3; j++){
-                if(j <= 3 && board[i][j] == piece && board[i][j+1] == piece && board[i][j+2] == piece && board[i][j+3] == piece){
-                    return true;
-                }
-                else if(j > 3 && board[i][j] == piece && board[i][j-1] == piece && board[i][j-2] == piece && board[i][j-3] == piece){
-                    return true;
-                }
-            }
+        if(row < 3 && this.board[row][column] == piece && board[row+1][column] == piece && board[row+2][column] == piece && board[row+3][column] == piece){
+            win = true;
         }
-        return false;
+        else if(row >= 3 && board[row][column] == piece && this.board[row-1][column] == piece && board[row-2][column] == piece && board[row-3][column] == piece){
+            win = true;
+        }
+        return win;
     }
-    public boolean checkWinDiagonal(String piece){
+    public boolean checkWinDiagonal(String piece,int column, int row){
         //check diagonal
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 3; j++){
-                if(i <= 4 && j <= 3 && board[i][j] == piece && board[i+1][j+1] == piece && board[i+2][j+2] == piece && board[i+3][j+3] == piece){
-                    return true;
-                }
-                else if(i > 4 && j > 3 && board[i][j] == piece && board[i-1][j-1] == piece && board[i-2][j-2] == piece && board[i-3][j-3] == piece){
-                    return true;
-                }
-            }
+        if(row < 3 && column >= 3 && board[row][column] == piece && board[row+1][column-1] == piece && board[row+2][column-2] == piece && board[row+3][column-3] == piece){
+            return true;
         }
-        for(int i = 0; i < 4; i++){
-            for(int j = 3; j < 6; j++){
-                if(i <= 4 && j >= 3 && board[i][j] == piece && board[i+1][j-1] == piece && board[i+2][j-2] == piece && board[i+3][j-3] == piece){
-                    return true;
-                }
-                else if(i >= 4 && j <= 3 && board[i][j] == piece && board[i-1][j+1] == piece && board[i-2][j+2] == piece && board[i-3][j+3] == piece){
-                    return true;
-                }
-            }
+        else if(row >=3 && column >=2 && board[row][column] == piece && board[row-1][column+1] == piece && board[row-2][column+2] == piece && board[row-3][column+3] == piece){
+            return true;
+        }
+        else if(row <= 2 && column <=3 && board[row][column] == piece && board[row+1][column+1] == piece && board[row+2][column+2] == piece && board[row+3][column+3] == piece){
+            return true;
+        }
+        else if(row >= 3 && column >= 3 && board[row][column] == piece && board[row-1][column-1] == piece && board[row-2][column-2] == piece && board[row-3][column-3] == piece){
+            return true;
         }
         return false;
     }
     //method to check if a player has won
-    public boolean checkWin(String piece){
+    public boolean checkWin(String piece, int column, int row){
+        boolean win = false;
         //check horizontal
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 6; j++){
-                if(board[i][j] == piece && board[i+1][j] == piece && board[i+2][j] == piece && board[i+3][j] == piece){
-                    return true;
-                }
-            }
+        if (win == false){
+            win = checkWinHorizontal(piece, column, row);
         }
-        //check vertical
-        for(int i = 0; i < 7; i++){
-            for(int j = 0; j < 3; j++){
-                if(board[i][j] == piece && board[i][j+1] == piece && board[i][j+2] == piece && board[i][j+3] == piece){
-                    return true;
-                }
-            }
+        if (win == false){
+            win = checkWinVertical(piece, column, row);
         }
         //check diagonal
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 3; j++){
-                if(board[i][j] == piece && board[i+1][j+1] == piece && board[i+2][j+2] == piece && board[i+3][j+3] == piece){
-                    return true;
-                }
-            }
+        if (win == false){
+            win = checkWinDiagonal(piece,column,row);
         }
-        for(int i = 0; i < 4; i++){
-            for(int j = 3; j < 6; j++){
-                if(board[i][j] == piece && board[i+1][j-1] == piece && board[i+2][j-2] == piece && board[i+3][j-3] == piece){
-                    return true;
-                }
-            }
-        }
-        return false;
+        return win;
     }
     public String result(boolean win){
         String message = "";
--- player.java
+++ player.java
@@ -1,105 +1,125 @@
 import java.io.*;
-import java.util.*;
 import java.net.*;
+import java.util.Random;
+import java.util.Scanner;
 
-public class player {
-    private DatagramSocket socket;
-    private boolean running;
-    private byte[] buf = new byte[256];
-    private int port_number;
+public class Player {
+    
     private InetAddress address;
+    private int port_number;
+    private DatagramSocket socket;
+    private boolean running = false;
+    private byte[] buf = new byte[256];
     private ServerSocket player1_socket;
     private Socket player2_socket1;
     private Scanner scanner = new Scanner(System.in);
-    //
-    public static String start(String[] args){
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.connection(start(args));  // Setup connection using args
+        player.run();  // Start running the game logic
+    }
+
+    public static String start(String[] args) {
         String broadcast_address = args[0];
         String broadcast_port = args[1];
-        return broadcast_address + ":" + broadcast_port;
+        return broadcast_address + ":" + broadcast_port;  // Return connection details
     }
-    public void connection(String connection_details){
+
+    public void connection(String connection_details) {
         String[] connectionData = connection_details.split(":");
         try {
-            address = InetAddress.getByName(connectionData[0]); // Get the IP address of the server
-            port_number = Integer.parseInt(connectionData[1]); // Get the port number of the server
+            address = InetAddress.getByName(connectionData[0]);  // Get the IP address of the server
+            port_number = Integer.parseInt(connectionData[1]);  // Get the port number
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
     }
-    public void run(){
-        try{
-            socket = new DatagramSocket(port_number);
-            socket.setSoTimeout(30000); // Wait for 30 seconds before timing out
+
+    public void run() {
+        try {
+            socket = new DatagramSocket(port_number);  // Create a new socket
+            socket.setSoTimeout(10000);  // Set timeout for receiving data
             System.out.println("Server is running on port " + port_number);
             running = true;
-            while(running){
-                DatagramPacket packet = new DatagramPacket(buf, buf.length); // Create a packet to receive data
-                try{
-                    socket.receive(packet); // Receive data from the player
-                    String received = new String(packet.getData(), 0, packet.getLength()); // Convert the data to a string
-                    String[] receivedData = received.split(":");
+            while (running) {
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);  // Create packet to receive data
+                try {
+                    socket.receive(packet);  // Receive data from the network
+                    String received = new String(packet.getData(), 0, packet.getLength());  // Convert to string
                     System.out.println("Received from client: " + received);
-                    if(received.startsWith("NEW GAME:")){
-                        int client_port = Integer.parseInt(receivedData[1]); // Get the port number of the player
-                        address = packet.getAddress(); // Get the IP address of the player
-                        playAsPlayer2(client_port, address); // Play as player 2
-                    }
-                    else{
+
+                    if (received.startsWith("NEW GAME:")) {
+                        int client_port = Integer.parseInt(received.split(":")[1]);  // Get client port
+                        address = packet.getAddress();  // Get client IP address
+                        playAsPlayer2(client_port, address);  // Initiate as Player 2
+                    } else {
                         continue;
                     }
                 } catch (SocketTimeoutException e) {
-                    playAsPlayer1(); // Play as player 1 if no player is found
-                }   
+                    System.out.println("No player found. Starting new game as Player 1.");
+                    playAsPlayer1();  // If no player is found, start as Player 1
+                }
             }
-            socket.close();
-        } catch (Exception e){
+            socket.close();  // Close the socket when done
+        } catch (BindException e) {
+           playAsPlayer1();  // If port is already in use, start as Player 1
+        }catch (Exception e) {
             e.printStackTrace();
         }
-    }
-    public int getPort(){
-        Random random = new Random();
-        int port = random.nextInt(9000,9101); // Generate a random port number between 9000 and 9100
-        return port;
     }
 
-    //checks if the message is a new game message
-    public int newGameMessage(String message){
-        int port = 0;
-        if(message.startsWith("NEW GAME:")){
-            String[] messageData = message.split(":");
-            port = Integer.parseInt(messageData[1]); // Get the port number from the message
-        }
-        return port;
+    public int getPort() {
+        Random random = new Random();
+        return random.nextInt(9000, 9101);  // Generate a random port number between 9000 and 9100
     }
-    public void playAsPlayer2(int port, InetAddress address){
-        try{
-            Socket player2_socket = new Socket(address, port); // Create a new socket to connect to the player
-            PrintWriter out = new PrintWriter(player2_socket.getOutputStream(), true); // Create a new output stream
-            BufferedReader in = new BufferedReader(new InputStreamReader(player2_socket.getInputStream())); // Create a new input stream
+
+    public void playAsPlayer2(int port, InetAddress address) {
+        try {
+            Socket player2_socket = new Socket(address, port);  // Connect to Player 1's server socket
+            System.out.println("Connected to Player 1 on port " + port);
+            PrintWriter out = new PrintWriter(player2_socket.getOutputStream(), true);  // Create output stream
+            BufferedReader in = new BufferedReader(new InputStreamReader(player2_socket.getInputStream()));  // Input stream
+
+            // Here you could add the game logic for Player 2's interaction (reading moves, sending responses, etc.)
+            // Example:
+            String receivedMessage = in.readLine();  // Read data from Player 1
+            System.out.println("Player 1 move: " + receivedMessage);
+            
+            // Close connection when done
             player2_socket.close();
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-        }
-    public void playAsPlayer1(){
-        int port = getPort(); // Get a random port number
-        try{
-            DatagramSocket socket = new DatagramSocket(); // Create a new socket
-            String message = "NEW GAME:" + port; // Create a new game message
-            byte[] buf = message.getBytes(); // Convert the message to a byte array
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port_number); // Create a new packet to send the message
-            socket.send(packet); // Send the message to the server
-            // Wait for player 2 to connect to the player 1
-            player1_socket = new ServerSocket(port); // Create a new server socket for the player
-            player2_socket1 = player1_socket.accept(); // Accept the connection from the other player
-            System.out.println("Which column would you like to drop your piece in?(1-7): ");
-            int column = scanner.nextInt(); // Get the column number from the player
-            PrintWriter out = new PrintWriter(player2_socket1.getOutputStream(), true); // Create a new output stream
-            String insertmessage = "INSERT:" + column; // Create a new insert message
-            out.println(insertmessage); // Send the insert message to the other player
-            socket.close(); // Close the socket
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public void playAsPlayer1() {
+        int port = getPort();  // Get random port number for Player 1's server socket
+        try {
+            DatagramSocket socket = new DatagramSocket();  // Create a new socket
+            String message = "NEW GAME:" + port;  // Broadcast the new game message
+            byte[] buf = message.getBytes();  // Convert message to byte array
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port_number);  // Packet to send
+            socket.send(packet);  // Send the packet to the network
+            
+            // Now, wait for Player 2 to connect
+            player1_socket = new ServerSocket(port);  // Server socket to accept incoming connections
+            System.out.println("Waiting for Player 2 to join...");
+            player2_socket1 = player1_socket.accept();  // Accept connection from Player 2
+            
+            // Interact with Player 2 - for example, sending the first move
+            System.out.println("Which column would you like to drop your piece in? (1-7): ");
+            int column = scanner.nextInt();  // Get the column number from Player 1
+            PrintWriter out = new PrintWriter(player2_socket1.getOutputStream(), true);  // Output stream to Player 2
+            String insertMessage = "INSERT:" + column;  // Send the move
+            out.println(insertMessage);
+            
+            // After sending the move, close the sockets
+            player2_socket1.close();  // Close connection with Player 2
+            player1_socket.close();  // Close server socket
+            socket.close();  // Close the DatagramSocket
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+}
